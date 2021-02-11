4213821d59c472fb4bbb6c9608ea85483bece5a1
Dongw1126 <topblade71@naver.com>
2021-02-12 02:45:12 +0900

    feat : setting apply

--- src/background.js
+++ src/background.js
@@ -1,4 +1,26 @@
 var arr = []
+var intTime = 10;
+var canceledItem = false;
+
+function timeBounder(t) {
+  ret = Number(t);
+  if(ret <= 0) {
+    ret = 0;
+  }
+  else if(ret > 10000){
+    ret = 10000;
+  }
+
+  return ret;
+}
+
+function stopAllInterval(arr) {
+  arr.forEach(function(element) {
+    clearInterval(element);
+  });
+  arr = []
+  return arr;
+}
 
 function restartDownload() {
   console.log("running");
@@ -10,35 +32,42 @@ function restartDownload() {
     limit: showMax
   }, function(results) {
     results.forEach((item) => {
-      if(item.canResume) {
-        chrome.downloads.resume(item.id, function(){});
+      if (item.canResume) {
+        chrome.downloads.resume(item.id, function() {});
       }
     });
   });
 }
 
 chrome.extension.onConnect.addListener(function(port) {
-  console.log("background : connected");
-
   port.onMessage.addListener(function(msg) {
-    if (msg == true) {
+    console.log(msg);
+    intTime = timeBounder(msg.sec);
+    canceledItem = msg.canceled;
+
+    chrome.storage.sync.set({
+      canceled: canceledItem,
+      sec : intTime
+    });
+
+    if (msg.turnOn == true) {
+      arr = stopAllInterval(arr);
       console.log("auto resume started");
 
-      chrome.storage.sync.set({turnOn: true});
+      chrome.storage.sync.set({
+        turnOn: true
+      });
 
       runFunc = setInterval(restartDownload, 1000);
       arr.push(runFunc);
-    }
-
-    else {
+    } else {
       console.log("auto resume stopped");
 
-      chrome.storage.sync.set({turnOn: false});
-
-      arr.forEach(function(element){
-          clearInterval(element);
+      chrome.storage.sync.set({
+        turnOn: false
       });
-      arr = []
+
+      arr = stopAllInterval(arr);
     }
   });
 });
--- src/popup.js
+++ src/popup.js
@@ -1,18 +1,45 @@
-var startSwitch = document.getElementById("start");
-
-startSwitch.onclick = function () {
+function sendMsg(_startSwitch, _canceledSett, _intervalSett) {
   var port = chrome.extension.connect({
-        name: "connect background"
-   });
-   port.postMessage(this.checked);
-};
+    name: "connect background"
+  });
+  port.postMessage({
+    turnOn: _startSwitch.checked,
+    canceled: _canceledSett.checked,
+    sec: _intervalSett.value
+  });
+}
 
-chrome.storage.sync.get(['turnOn'], function(result) {
-  console.log(result);
-  if(result.turnOn){
-    startSwitch.checked = true;
-  }
-  else {
-    startSwitch.checked = false;
-  }
-});
+window.onload = function() {
+  var startSwitch = document.getElementById("start");
+  var applyButton = document.getElementById("apply-button");
+  var canceledSett = document.getElementById("canceledItem");
+  var intervalSett = document.getElementById("intervalTime");
+
+  startSwitch.addEventListener("click", function() {
+    sendMsg(startSwitch, canceledSett, intervalSett);
+  });
+  applyButton.addEventListener("click", function() {
+    sendMsg(startSwitch, canceledSett, intervalSett);
+  });
+
+  chrome.storage.sync.get(['turnOn'], function(result) {
+    console.log(result);
+    if (result.turnOn) {
+      startSwitch.checked = true;
+    } else {
+      startSwitch.checked = false;
+    }
+  });
+
+  chrome.storage.sync.get(['canceled'], function(result) {
+    if (result.canceled) {
+      canceledSett.checked = true;
+    } else {
+      canceledSett.checked = false;
+    }
+  });
+
+  chrome.storage.sync.get(['sec'], function(result) {
+    intervalSett.value = result.sec;
+  });
+}
