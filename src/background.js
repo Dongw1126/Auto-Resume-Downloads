191befd6a7c4db92d7bd6bb0a8d8ca891f73c535
Dongw1126 <topblade71@naver.com>
2021-02-11 15:19:15 +0900

    setInterval test

--- src/background.js
+++ src/background.js
@@ -1,6 +1,21 @@
+var arr = []
+
 chrome.extension.onConnect.addListener(function(port) {
      console.log("background : connected");
      port.onMessage.addListener(function(msg) {
-        console.log(msg);
+        if(msg == true) {
+          runFunc = setInterval(function() {
+             console.log("running");
+          }, 1000);
+          arr.push(runFunc);
+          console.log(arr);
+        }
+        else {
+          console.log("stop");
+          arr.forEach(function(element){
+              clearInterval(element);
+          });
+          arr = []
+        }
      });
 });
