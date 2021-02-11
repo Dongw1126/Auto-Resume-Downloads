243fe099b1304ea4b2b1bcdd6145e37960c1878c
Dongw1126 <topblade71@naver.com>
2021-02-11 19:11:51 +0900

    apply chrome API

--- src/background.js
+++ src/background.js
@@ -1,21 +1,48 @@
 var arr = []
 
+function restartDownload() {
+  console.log("running");
+  for (var num = 0; num < 1000; num++) {
+    chrome.downloads.resume(num, function() {
+      console.log("resume : " + num);
+    });
+  }
+}
+
 chrome.extension.onConnect.addListener(function(port) {
-     console.log("background : connected");
-     port.onMessage.addListener(function(msg) {
-        if(msg == true) {
-          runFunc = setInterval(function() {
-             console.log("running");
-          }, 1000);
-          arr.push(runFunc);
-          console.log(arr);
-        }
-        else {
-          console.log("stop");
-          arr.forEach(function(element){
-              clearInterval(element);
-          });
-          arr = []
-        }
-     });
+  console.log("background : connected");
+  var showMax = 50;
+  var downloadItems = {};
+
+  port.onMessage.addListener(function(msg) {
+    if (msg == true) {
+      console.log("running");
+      chrome.downloads.search({
+        orderBy: ['-startTime'],
+        limit: showMax
+      }, function(results) {
+        console.log(results);
+        results.forEach((item) => {
+          if(item.canResume) {
+            chrome.downloads.resume(item.id, function(){});
+          }
+        });
+      });
+
+      /*runFunc = setInterval(function() {
+         console.log("running");
+         for(var num=0; num < 1000; num++) {
+           chrome.downloads.resume(num, function() {
+             console.log("resume : " + num);
+           });
+         }
+       });
+       arr.push(runFunc);*/
+    } else {
+      /*arr.forEach(function(element){
+          clearInterval(element);
+      });
+      arr = []*/
+    }
+  });
 });
