cefa5a51e4888e0dcecb6f9aec8fd5f28ca7505c
Dongw1126 <topblade71@naver.com>
2021-09-20 16:17:44 +0900

    refactor: add comments

--- src/background.js
+++ src/background.js
@@ -1,14 +1,14 @@
-/**  default download resume time interval */  
+/** default download resume time interval (in seconds) */  
 const DEFAULT_INTERVAL            = 3;
 
-/**  maximum length of log (in bytes) */  
+/** maximum length of log (in bytes) */  
 const MAX_LOG_BYTE              = 7000;
 
 
-/**  list of functions running in the background */  
+/** list of functions running in the background */  
 var background_function_array     = [];
 
-/**  time interval between scanning and resuming downloads */  
+/** time interval between scanning and resuming downloads */  
 var interval_for_check            = DEFAULT_INTERVAL;
 
 /** whether to resume paused downloads as well */  
@@ -79,24 +79,37 @@ function getLimitedByteLog(str) {
   return str;
 }
 
-
-
+/**
+ * logging :                      save logs to chrome.storage.local
+ * @param {String} str            log in string
+ * 
+ * Logs to the key value localSavedLog in chrome.storage.local
+ */
 function logging(str) {
   chrome.storage.local.get(['localSavedLog'], function(result) {
-    /* since the connection with popup.js may be disconnected, save logs in local storage */
     if (typeof result.localSavedLog == "undefined") {
       result.localSavedLog = "";
     }
+
     var updatedLog = result.localSavedLog + str + "\n\n";
     updatedLog = getLimitedByteLog(updatedLog);
+
     chrome.storage.local.set({
       localSavedLog: updatedLog
     });
   });
 }
 
+/**
+ * resumeDownload :               function to resume downloads
+ * @param {Array} DownloadItems   array of download information
+ * 
+ * A function to resume downloads that meet the conditions 
+ * while checking DownloadItem passed from chrome.downloads.search one by one
+ */
 function resumeDownload(DownloadItems) {
   DownloadItems.forEach(function(item) {
+    console.log(item);
     crxd = chrome.downloads.resume;
     if (item.canResume) {
       if (!item.paused || resume_on_paused) {
@@ -108,10 +121,18 @@ function resumeDownload(DownloadItems) {
   });
 }
 
-function downloadManager() {
-  chrome.downloads.search({}, resumeDownload);
+/**
+ * downloadManager :              search for downloads and call resumeDownload
+ * @param {Object} query          Download item search criteria
+ */
+function downloadManager(query={}) {
+  chrome.downloads.search(query, resumeDownload);
 }
 
+/**
+ * autoResume :                   automatically resumes the downloads according to the toggle value
+ * @param {Boolean} toggle        whether Resume Downloads is currently turned on
+ */
 function autoResume(toggle) {
   if (toggle) {
     background_function_array = stopAllIntervals(background_function_array);
@@ -121,44 +142,46 @@ function autoResume(toggle) {
     background_function_array = stopAllIntervals(background_function_array);
   }
 
+  /* resave current options */
   chrome.storage.local.set({
     running: toggle
   });
 }
 
-// load options
+/* load an option for resuming paused downloads */
 chrome.storage.local.get(['paused'], function(result) {
   resume_on_paused = result.paused;
 });
 
+/* load an option for download resume time interval */
 chrome.storage.local.get(['sec'], function(result) {
   interval_for_check = timeBoundary(result.sec);
 });
 
-// if last state is on, start Auto resume
+/* loads an option for whether auto-resume-download works */
 chrome.storage.local.get(['running'], function(result) {
   if (result.running) {
     autoResume(true);
   }
 });
 
-// connection with popup.js
+/* connection with popup.js */
 chrome.extension.onConnect.addListener(function(port) {
   port.onMessage.addListener(function(message) {
-    // get options from popup
+    /* get options from popup */
     interval_for_check = timeBoundary(message.sec);
     resume_on_paused = message.paused;
 
+    /* resave current options */
     chrome.storage.local.set({
       paused: resume_on_paused,
       sec: interval_for_check
     });
 
+    /* auto resume */
     if (message.running == true) {
-      // auto resume start
       autoResume(true);
     } else {
-      // auto resume stop
       autoResume(false);
     }
   });
